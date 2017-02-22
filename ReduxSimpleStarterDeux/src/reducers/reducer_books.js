654820eb0c7093d41e0b915ddc92eda9a7cdf663
Scott Nash <snash@kbsp.com>
2017-02-22 18:20:03 -0500

    adding new test projects

---
+++ ReduxSimpleStarterDeux/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    { title: 'The Hobbit', pages: 1000},
+    { title: 'Harry Potter and the Philospher\'s Stone', pages: 200},
+    { title: 'The Great Gatsby', pages: 300},
+    { title: 'Catcher in the Rye', pages: 325}
+  ]
+}
